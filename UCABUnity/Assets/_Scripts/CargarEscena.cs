3368ee8929eb48afa137e4cb073affed671aa05f
GerLC <39887800+GerLC@users.noreply.github.com>
2019-05-08 01:57:40 -0400

    2#
    Se corrigio lo anterior

---
+++ UCABUnity/Assets/_Scripts/CargarEscena.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargarEscena : MonoBehaviour
+{
+    // public void CargarScene(int escena) 
+    //[SerializeField]
+   // private string escena;
+
+    public void CargarScene()
+    {
+        //Application.LoadLevel(escena);    
+        if (Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
---
+++ UCABUnity/Assets/_Scripts/MenuPausa.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuPausa : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       // if (Input.GetKeyDown(KeyCode.Escape))
+       if (Input.touchCount > 0)
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+    // Nota = Es para teclado de pc. No es obligatorio, creo.
+    
+     public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void Pause ()
+    {
+ 
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    [SerializeField]
+    private string escena;
+    public void CargarScene(string escena)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(escena);
+    }
+
+
+
+}
