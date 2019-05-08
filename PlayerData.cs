57482607c2b02f2a32a4cdc1c2c8d6604e36abf0
GerLC <39887800+GerLC@users.noreply.github.com>
2019-05-08 00:07:35 -0400

    1

---
+++ PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+
+{ 
+    public float puntajemax;
+    public float puntajeactual;
+   
+    // Player va a ser la clase que tiene los datos del juegador
+    public PlayerData (/*Player player*/)
+    {
+      /*  puntajemax = player.puntajemax;
+        puntajeactual = player.puntajeactual;*/
+    }
+}
