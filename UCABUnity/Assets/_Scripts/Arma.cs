3f23cf65f5dd249d0db4eb9208b2822ca2795451
OmarElFakih <omarelfakih97@gmail.com>
2019-05-08 09:44:21 -0400

    ScriptableAdded

---
+++ UCABUnity/Assets/_Scripts/Arma.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : ScriptableObject
+{
+    public float fireRate;
+
+    public virtual void Disparar(Transform transform)
+    {
+        Debug.Log("recarga abstracta");
+    }
+
+    public virtual void Recargar(Transform transform)
+    {
+        Debug.Log("recarga abstracta");
+    }
+
+}
