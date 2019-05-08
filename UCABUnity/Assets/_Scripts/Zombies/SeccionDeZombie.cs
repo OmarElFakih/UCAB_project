37815bd1ff2e56b5f556306c12644b2888bdde51
OmarElFakih <omarelfakih97@gmail.com>
2019-05-08 11:16:12 -0400

    Zombie Logic Started

---
+++ UCABUnity/Assets/_Scripts/Zombies/SeccionDeZombie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeccionDeZombie : ShootingTarget
+{
+    [SerializeField] private float dmMultiplier = 0f;
+
+    private Zombie parent = null;
+
+    private void Start()
+    {
+        parent = GetComponentInParent<Zombie>();
+    }
+
+    public override void GetShot(bullet bullet)
+    {
+        
+    }
+}
---
+++ UCABUnity/Assets/_Scripts/Zombies/Zombie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [SerializeField] private float MaxHealth;
+    private float currentHealth;
+
+
+    private void Update()
+    {
+        
+    }
+
+    private void AIUpdtaer() {
+        //
+
+    }
+}
