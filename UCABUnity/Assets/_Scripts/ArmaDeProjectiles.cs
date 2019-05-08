b10d070ccd5b270b84dfe765f339e1cbc3adf608
OmarElFakih <omarelfakih97@gmail.com>
2019-05-08 16:41:24 -0400

    Revert "Revert "ScriptableAdded""
    This reverts commit b434072b69c2d48bd3eb73dbab762fe2a7ea1640.

---
+++ UCABUnity/Assets/_Scripts/ArmaDeProjectiles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu (menuName = "Arma de Projectiles")]
+public class ArmaDeProjectiles : Arma
+{
+
+    public GameObject bulletPrefab;
+
+    public override void Disparar(Transform transform)
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+}
