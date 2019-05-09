d40e03f4839ffb6cd9251f5791b47ed097ed935b
GerLC <39887800+GerLC@users.noreply.github.com>
2019-05-09 02:21:08 -0400

    5# ~ Puntaje Actual / Maximo

---
+++ UCABUnity/Assets/_Scripts/ControldeJuego.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ControldeJuego : MonoBehaviour
+{
+
+    public static ControldeJuego instance;
+
+    public static bool GamePaused = false;
+
+    public GameObject ControldeJuegoUI;
+
+    private DatosGuardados ControldePuntaje;
+
+
+      void Awake()
+      {
+
+        instance = this;
+        /*  if (instance == null)
+          {
+              instance = this;
+              DontDestroyOnLoad(this.gameObject);
+          }
+          else
+          {
+              if (instance != this)
+                  Destroy(this.gameObject);
+          }*/
+      } 
+
+
+
+    void Start()
+    {
+        ControldePuntaje = FindObjectOfType<DatosGuardados>();
+    }
+
+    public void borrarPuntaje()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        ControldePuntaje.puntajeMaximo = 0;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {      
+    }
+
+
+
+    public void Pausar ()
+    {
+        if (GamePaused)
+        {
+            ControldeJuegoUI.SetActive(false);
+            Time.timeScale = 1f;
+            GamePaused = false;
+        }
+        else
+        {
+            ControldeJuegoUI.SetActive(true);
+            Time.timeScale = 0f;
+            GamePaused = true;
+        }
+        
+    }
+
+
+    [SerializeField]
+    private int escena;
+    public void CargarEscena(int escena)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(escena);
+    }
+
+
+
+}
