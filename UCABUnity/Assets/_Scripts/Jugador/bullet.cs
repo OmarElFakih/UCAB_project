648b1c0c1d44d1467ea27b312f5a0bd29d272d47
OmarElFakih <omarelfakih97@gmail.com>
2019-05-11 17:32:32 -0400

    carpeta leccion agregada

--- UCABUnity/Assets/_Scripts/Jugador/bullet.cs
+++ UCABUnity/Assets/_Scripts/Jugador/bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-
+    
     [SerializeField] private float speed = 0f;
     public float damage;
 
---
+++ UCABUnity/Assets/_Scripts/Lecciones/Triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangulo : MonoBehaviour
+{
+
+    private new SpriteRenderer renderer = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.color = Color.yellow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
